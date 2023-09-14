026638db2208629d2de014ede3996016dcf0336f
montleee <144028935+montleee@users.noreply.github.com>
2023-09-14 17:28:55 +0300

    commit text added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.scss';
-import Title from './Components/Title/Title';
+import "./App.scss";
+import Title from "./Components/Title/Title";
+import Text from "./Components/Text/Text";
 function App() {
   return (
     <div className="App">
-        <Title></Title>
-        
+      <Title></Title>
+      <Text></Text>
     </div>
   );
 }
