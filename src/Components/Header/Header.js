66348bf02a2b73db16ec98e2e768c63f48319bd3
merdernoty <133769530+merdernoty@users.noreply.github.com>
2023-09-15 01:28:51 +0300

    commit Clicker beta 1.0

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Modal } from "./Modal/Modal";
 import "./Header.scss";
+
 function Header() {
-  const [Click, setClick] = useState(0)
+  const [clickCount, setClickCount] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(10);
+  const [isCounting, setIsCounting] = useState(false);
+  const [modalActive, setModalActive] = useState(false);
+
+  useEffect(() => {
+    let timer;
+
+    const countdown = () => {
+      if (timeLeft > 0) {
+        setTimeLeft(timeLeft - 1);
+      } else {
+        // Когда время истекло, устанавливаем modalActive в true
+       setModalActive(true);
+        clearInterval(timer);
+        setIsCounting(false);
+        setClickCount(0);
+        setTimeLeft(10);
+      }
+    };
+
+    if (isCounting) {
+      timer = setInterval(countdown, 1000);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isCounting, timeLeft]);
+
+  const handleClick = () => {
+    if (!isCounting) {
+      setIsCounting(true);
+    }
+    setClickCount(clickCount + 1);
+  };
 
   return (
     <div className="WrapperMain">
       <div className="WrapperHeader">
         <div className="WrapperBtn">
-          <button onClick={() =>setClick(Click + 1)}className="Btn">{Click}</button>
+          <button onClick={handleClick} className="Btn"></button>
+        </div>
+        <div className="WrapperPar">
+          <div className="Parametr1">
+            <a>
+              Timer: <b>{timeLeft === 0 ? 0 : timeLeft}</b>
+            </a>
           </div>
-          <div className="WrapperPar">
-            <div className="Parametr1">1</div>
-            <div className="Parametr2">2</div>
-            <div className="Parametr3">3</div>
+          <div className="Parametr2">{clickCount}</div>
+          <div className="Parametr3">
+            <a>
+              Score: <b>{clickCount}</b>
+            </a>
+          </div>
         </div>
       </div>
+      {modalActive && <Modal active={modalActive} setActive={setModalActive} />}
     </div>
   );
 }
