796a38d345815cb2d3c79bfd869f191107f524f5
montleee <144028935+montleee@users.noreply.github.com>
2023-09-14 17:58:42 +0300

    commit footer add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 import Title from "./Components/Title/Title";
 import Text from "./Components/Text/Text";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div className="App">
       <Title></Title>
       <Text></Text>
+      <Footer></Footer>
     </div>
   );
 }
