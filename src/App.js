d5511253eda1a18294ecb5dd66829d40c156863f
montleee <144028935+montleee@users.noreply.github.com>
2023-09-20 00:14:10 +0300

    commit themecss

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Title from "./Components/Title/Title";
 import Text from "./Components/Text/Text";
 import Footer from "./Components/Footer/Footer";
 import Header from './Components/Header/Header';
+import Theme from './Components/Theme/Theme';
 function App() {
   return (
     <div className="App"> 
       <Title></Title>
+      <Theme></Theme>
       <Header></Header>
       <Text></Text>
       <Footer></Footer>
+
       </div>
  );
 };
